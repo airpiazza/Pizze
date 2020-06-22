a44f06a080fe6f64ec5a9d884a64150949ff48a5
airpiazza <nick@piazzaonline.com>
2020-06-21 23:03:47 -0700

    Login and Sign Up UI

--- app/src/main/java/com/nicholaspiazza/pizze/MainActivity.kt
+++ app/src/main/java/com/nicholaspiazza/pizze/MainActivity.kt
@@ -9,10 +9,16 @@ import android.widget.RadioButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView3.setOnClickListener{
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
     }
 }
