a54f5df0939763c493939bd514db44d6e7ef6af4
airpiazza <nick@piazzaonline.com>
2020-07-21 17:44:38 -0700

    post activity ui

--- app/src/main/java/com/nicholaspiazza/pizze/Post.kt
+++ app/src/main/java/com/nicholaspiazza/pizze/Post.kt
@@ -2,10 +2,15 @@ package com.nicholaspiazza.pizze
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Post : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+        val goBackButton = findViewById<Button>(R.id.goback)
+        goBackButton.setOnClickListener {
+            finish()
+        }
     }
 }
